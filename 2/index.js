40dc6297e83d953ff1fd85b6db82fb83c6c450be
Gulburak1 <116088697+Gulburak1@users.noreply.github.com>
2022-12-05 19:40:09 +0600

    Add files via upload

---
+++ 2/index.js
@@ -0,0 +1,22 @@
+let object = {
+    name: "oleg",
+    age: 15,
+    eyeColor: "blue"
+}
+console.log("Загрузка")
+let promise = new Promise
+(function(resolve){
+    // let jsonData = JSON.stringify(object);
+    resolve(jsonData)
+}).then(function(res){
+    setTimeout(function(){
+        console.log("идет обработка данных")
+    },1000)
+setTimeout(function(){
+    let newobject = JSON.parse(res)
+    console.log(newobject)
+},2000)
+}).catch((err)=> {
+    console.log("Ошибка")
+})
+
